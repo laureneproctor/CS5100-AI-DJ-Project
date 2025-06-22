Fix the wrong index mapping and the return type of get_playlist()

k_means.py
@@ -7,9 +7,9 @@ import data_prep as prep
 import pca
 import pickle
 
-K = 8
-LABEL_MAP = {0: "Happy", 1: "Sad", 2: "Tense", 3: "Dark" ,
-             4: "Wild", 5: "Chill", 6: "Powerful", 7: "Party"}
+K = 6
+LABEL_MAP = {0: "Happy", 1: "Sad", 2: "Powerful", 3: "Chill" ,
+             4: "Tense", 5: "Party"}
 
 def euclidean_dist(v1, v2):
     inner = 0
@@ -21,14 +21,14 @@ def euclidean_dist(v1, v2):
 # then pick points that are as far away from one another as possible
 def create_centroids(k, data):
     # randomly choose the first point
-    centroids = [random.choice(data)]
+    centroids = [random.choice(data)[1]]
     # add the point whose minimum distance from the selected points as large as possible
     while len(centroids) < k:
         min_distances = []
-        for p in data:
+        for _, p in data:
             distances = [euclidean_dist(p, c) for c in centroids]
             min_distances.append(min(distances))
-        next_c = data[np.argmax(min_distances)]
+        next_c = data[np.argmax(min_distances)][1]
         centroids.append(next_c)
     return(centroids)
 
@@ -46,7 +46,7 @@ def create_clusters(data, centroids):
     for i in range(k):
         clusters.append([])
 
-    for i, p in enumerate(data):
+    for i, p in data:
         distances = [euclidean_dist(p, c) for c in centroids]
         # use argmin to speed up this process
         best_cluster = np.argmin(distances)
@@ -69,7 +69,7 @@ def k_means_cluster(k, data):
             new_c = update_centroid([feature for (_, feature) in cluster])
             if new_c is not None:
                 new_centroids.append(new_c)
-            differences.append(euclidean_dist(old_c, new_c) < threshold)
+                differences.append(euclidean_dist(old_c, new_c) < threshold)
         converged = all(differences)
         centroids = new_centroids
         iteration += 1
@@ -79,16 +79,23 @@ def find_radius(clusters, centroids):
     radius = []
     for i in range(len(centroids)):
         features = [feature for (_, feature) in clusters[i]]
-        radius.append(np.max([euclidean_dist(data_point, centroids[i]) for data_point in features]))
+        if len(features) == 0:
+            radius.append(0)
+        else:
+            radius.append(np.max([euclidean_dist(data_point, centroids[i]) 
+                                  for data_point in features]))
     return radius
 
 def find_k():
     train_data = pca.TRAIN_DATA
     avg_radius = []
     for k in range(2, 16):
-        clusters, centroids = k_means_cluster(k, train_data)
-        radius = find_radius(clusters, centroids)
-        avg_radius.append(np.mean(radius))
+        radius_list = []
+        for _ in range(5):
+            clusters, centroids = k_means_cluster(k, train_data)
+            radius = find_radius(clusters, centroids)
+            radius_list.append(np.mean(radius))
+        avg_radius.append(np.mean(radius_list))
     plt.plot(range(2, 16), avg_radius)
     plt.xlabel("Number of clusters")
     plt.ylabel("Average Radius")

main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import data_prep as prep
+import random
 import pca
 import k_means
 
@@ -10,7 +11,7 @@ LABEL_MAP = k_means.LABEL_MAP
 K = k_means.K
 CLUSTERS, CENTROIDS = k_means.CLUSTERS, k_means.CENTROIDS
 
-sampled_test_data = TEST_DATA[np.random.choice(len(TEST_DATA), 100, replace=False)]
+sampled_test_data = random.sample(TEST_DATA, 60)
 
 def show_options():
     print("Moods availabe to choose from: ")
@@ -23,8 +24,19 @@ def get_playlist(input_songs=sampled_test_data):
     while cluster not in valid_cluster:
         show_options()
         cluster = input("Choose the mood of your playlist (type the number): ")
-    playlist = [SONG_MAP[idx] for (idx, _) in 
-                k_means.create_clusters(input_songs, CENTROIDS)[int(cluster)-1]]
-    print(len(playlist))
-    print(playlist)
+    # a playlist of track name and artist, print out for debugging 
+    cluster_output = k_means.create_clusters(input_songs, CENTROIDS)
+    playlist_info = [SONG_MAP[idx] for (idx, _) in cluster_output[int(cluster)-1]]
+    raw_playlist = cluster_output[int(cluster)-1]
+    # transfer the pcas back to original features to be used in local search
+    pcas = [feature for (_, feature) in raw_playlist]
+    normalized_features = pca.MY_PCA.inverse_transform(pcas)
+    original_features = prep.ss.inverse_transform(normalized_features)
+    playlist = [(raw_playlist[i][0], original_features[i]) for i in range(len(raw_playlist))]
+    print(len(playlist_info))
+    print(playlist_info)
+    # The data structure is (index, array["danceability", "energy", "key", "loudness", "mode", "valence", "tempo"])
     return playlist
+
+if __name__ == "__main__":
+    get_playlist()